b0d4ab2ac470c5f2fdfcf9241b7a77b230e77ce1
Xavier <inscription@mobius.zone>
2021-05-28 17:51:20 +0200

    First serializer version

---
+++ src/ProseMirror.Serializer/ProseMirrorSerializer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using ProseMirror.Model;
+using System.Collections.Generic;
+
+namespace ProseMirror.Serializer
+{
+    public class ProseMirrorSerializer
+    {
+        public static string Serialize(Node proseMirrorNode) => JsonConvert.SerializeObject(proseMirrorNode, new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters = new List<JsonConverter>() { new StringEnumConverter() }
+        });
+        public static Node Deserialize(string jSon) => JsonConvert.DeserializeObject<Node>(jSon, new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters = new List<JsonConverter>() { new StringEnumConverter() }
+        });
+    }
+}
