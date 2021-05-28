98a2a1665038ff188e740eaf20cf706fbebe56b4
Xavier <inscription@mobius.zone>
2021-05-28 17:57:37 +0200

    Console program for testing and example purpose

---
+++ src/ProseMirror.Model.Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ProseMirror.Model.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText("example.json");
+            var proseMirrorNode = Serializer.ProseMirrorSerializer
+                .Deserialize(json);
+            var result = Serializer.ProseMirrorSerializer
+                .Serialize(proseMirrorNode);
+        }
+    }
+    public class Container
+    {
+        
+        public Container()
+        {
+            
+        }
+    }
+    public class ReferenceAttributes : NodeAttributes
+    {
+        public string DecorationChar { get; set; }
+        public string ReferenceDocUid { get; set; }
+        public string Text { get; set; }
+        public bool? Unknown { get; set; }
+        public int? AssignmentId { get; set; }
+    }
+}
